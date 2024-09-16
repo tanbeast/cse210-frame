e5fa35d15badb0766f767d7d0d51d793ba32bac7
Kade O'Crowley <kadeocrowley@gmail.com>
2024-09-16 14:27:25 -0600

    week 1 work! im learning C#!!

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("what is your first name? ");
+        string Fname = Console.ReadLine();
+        Console.Write("what is your last name? ");
+        string lname = Console.ReadLine();
+        Console.Write($"Your name is {lname}, {Fname} {lname}");
     }
 }
