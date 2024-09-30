01bd104ec41b0c5662acd0474f0b6c260e821f70
Kade O'Crowley <kadeocrowley@gmail.com>
2024-09-30 11:23:30 -0600

    week 3 work

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2016;
+        job1._endYear = 2024;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Software Engineer";
+        job2._company = "Apple";
+        job2._startYear = 2017;
+        job2._endYear = 2022;
+
+        Resume myResume = new Resume();
+        myResume._name = "Kade O'Crowley";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
