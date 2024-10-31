3ff04b6ecef59c0c48bc57d4a1cc51fe311a52bf
Kade O'Crowley <kadeocrowley@gmail.com>
2024-10-31 09:12:39 -0600

    prove 4

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -1,9 +1,16 @@
 using System;
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Learning04 World!");
+class Program{
+    static void Main(string[] args){
+        Assignment one = new Assignment("David Walls", "Multiplication");
+        Console.WriteLine(one.GetSummary());
+
+        Math two = new Math("kristy Wood", "Fractions", "6.8", "3-29");
+        Console.WriteLine(two.GetSummary());
+        Console.WriteLine(two.GetHomeworkList());
+
+        Writing three = new Writing("Mary Mack", "History", "The Causes of Civl War");
+        Console.WriteLine(three.GetSummary());
+        Console.WriteLine(three.GetWritingInformation());
     }
 }
